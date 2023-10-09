52701a554137f57c003308b8a576d9ea94be82b4
Daria Liferova <liferova.daria@gmail.com>
2023-10-09 17:42:40 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen text-4xl text-blue-400 flex items-center justify-center">
+          Hello, World
+      </div>
+    </>
+  )
+}
+
+export default App
