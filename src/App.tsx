997e48f7b4c4e5503982839ca85affe58cf048c6
Daria Liferova <liferova.daria@gmail.com>
2024-02-23 13:35:53 +0100

    Change styles for main page container

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import Delivery from "./pages/delivery/Delivery.tsx"
 function App() {
   return (
     <>
-      <div className="w-full h-screen bg-[#f6f8fb] text-slate-950">
-        <header className="w-full bg-[#67a0f6] h-10" />
-        <div className="pt-[40px] pl-[20px] pr-[20px] pb-[80px]">
-          <Delivery />
-        </div>
+      <header className="w-full bg-[#67a0f6] h-10" />
+      <div className="wrapper">
+        <Delivery />
       </div>
     </>
   )
--- src/components/deliveryForm/validationSchema.ts
+++ src/components/deliveryForm/validationSchema.ts
@@ -1,19 +1,16 @@
 import * as Yup from "yup"
-import { DeliveryVariants } from "../../types/enums.ts"
 
-export const validationSchema = {
+export const validationSchema = Yup.object().shape({
   deliveryVariant: Yup.string().required("Выберите вариант доставки"),
   cityId: Yup.string().required("Field is required"),
-  pickUpPointAddress: Yup.object().when("deliveryVariant", {
-    is: (deliveryVariant: DeliveryVariants) =>
-      deliveryVariant === DeliveryVariants.PickUp,
-    then: Yup.object().shape({
-      address: Yup.string().required("Укажите адрес для самовывоза"),
-      coordinates: Yup.string().required("Укажите координаты для самовывоза"),
-    }),
-    otherwise: Yup.object().shape({
-      address: Yup.string().nullable(),
-      coordinates: Yup.string().nullable(),
-    }),
-  }),
-}
+  // pickUpPointAddress: Yup.object().when(["deliveryVariant"], {
+  //   is: DeliveryVariants.PickUp,
+  //   then: Yup.object().shape({
+  //     address: Yup.string().required("Укажите адрес для самовывоза"),
+  //   }),
+  //   otherwise: Yup.object().shape({
+  //     address: Yup.string().nullable(),
+  //     coordinates: Yup.string().nullable(),
+  //   }),
+  // }),
+})
